d03e9b89f21bcda2b2446d7497f1f35e457a89e3
Jadson Pimentel <jadsonpimentel10@gmail.com>
2023-05-07 20:42:17 -0300

    Mochila de Viagem

--- js/main.js
+++ js/main.js
@@ -1,16 +1,61 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
 
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+
+itens.forEach((elemento) => {
+  criaElemento(elemento)
+})
 
 form.addEventListener("submit", (evento) => {
   // INTERROMPE O EVENTO PADRÃO POST DO FORMULARIO
   evento.preventDefault()
 
-   criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+  const nome = evento.target.elements['nome']
+  const quantidade = evento.target.elements['quantidade']
+
+   // 
+   const itemAtual = {
+    "nome": nome.value,
+    "quantidade": quantidade.value
+  }
+ 
+  // COLETANDO O ELEMENTO CASO ELE JÁ EXISTE NA LISTA
+  const ifExists = itens.find(elemento => elemento.nome === nome.value)
+
+  // CONDIÇÃO CASO ELE JÁ EXISTA
+  if (ifExists) {
+    itemAtual.id = ifExists.id
+
+    atualizaElemento(itemAtual)
+
+    itens[ifExists.id] = itemAtual
+
+  } else {
+    itemAtual.id = itens.length
+
+    criaElemento(itemAtual)
+    
+    // ITERANDO CADA ITEM E SALVANDO NO ARRAY
+    itens.push(itemAtual)
+
+  }
+
+
+
+  // SALVANDO INFORMAÇÃO NO LOCALSTORAGE DO NAVEGADOR
+  localStorage.setItem("itens", JSON.stringify(itens))
+  
+
+  // LIMPANDO CONTEUDO DO FORMULARIO APOS ENVIADO
+  nome.value = ""
+  quantidade.value = ""
+
 })
 
   //CRIAR UM ELEMENTO ATRAVES DO JAVASCRIPT
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
 
   // CRIANDO UM NOVO ELEMENTO NO HTML E ADICIONANDO UMA CLASSE AO NOVO ELEMENTO 
   const novoItem = document.createElement('li')
@@ -18,12 +63,25 @@ function criaElemento(nome, quantidade) {
 
   // CRIANDO UM NOVO ELEMENTO NO HTML E ADICIONANDO UMA CLASSE AO NOVO ELEMENTO 
   const numeroItem = document.createElement('strong')
-  numeroItem.innerHTML = quantidade
+  numeroItem.innerHTML = item.quantidade
 
+  // CRIA UM NOVO ELEMENTO NO HTML COMO DATA ATRIBUTES
+  numeroItem.dataset.id = item.id
+
+  // INCLUINDO NUMEROITEM COMO UM DEPENDENTE DE NOVO ITEM E INSERINDO NO HTML MAIS O NOME DO ITEM
   novoItem.appendChild(numeroItem)
-  novoItem.innerHTML += nome
+  novoItem.innerHTML += item.nome
 
   lista.appendChild(novoItem)
 
   console.log(lista)
+
 }
+
+function atualizaElemento(item) {
+  document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+
+
+}
+
+
